6a289fd2eed32446389e6b3300c0b1d4f8e041e0
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-24 05:00:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,16 @@
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  
   languages.each {|key, value|
-    key.each {|key, value|
+    value.each {|key, value|
       new_hash[key] = value
     }
   }
-  
-  
+  new_hash.each {|key, value|
+    if languages[:oo].has_key?(key)
+      new_hash[key][:style] = [:oo]
+    elsif languages[:functional].has_key?(key)
+      new_hash[key][:style] = [:functional]
+    end
+  }
 end
