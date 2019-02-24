0eabbbf6171073b94cb12076e502ef9c91aee056
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-24 04:51:21 +0000

    Automatically backed up by Learn

--- prac.rb
+++ prac.rb
@@ -37,9 +37,8 @@ languages = {
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  
   languages.each {|key, value|
-    key.each {|key, value|
+    value.each {|key, value|
       new_hash[key] = value
     }
   }
